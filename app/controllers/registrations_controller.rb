c803ad8795f2a65dab5a8daa01a7b2d3da9096a8
Gustavo Carvalho <gsc13@tuta.io>
2021-04-06 14:26:20 -0300

    Add user model with devise

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,7 @@
+class RegistrationsController < Devise::RegistrationsController
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+  end
+end
