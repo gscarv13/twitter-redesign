c5eeb7677878f5535ea0e0fb32fd5e3496cf3f51
Gustavo Carvalho <gsc13@tuta.io>
2021-04-06 20:05:22 -0300

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  validates :text, presence: true, length: { minimum: 10, maximum: 200 }
+
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :full_name, presence: true, length: { minimum: 3, maximum: 20 }
+
+  has_many :posts, class_name: 'Post', foreign_key: 'author_id'
+  has_many :followers, class_name: 'Following', foreign_key: 'follower_id'
+  has_many :followed, class_name: 'Following', foreign_key: 'followed_id'
 end
