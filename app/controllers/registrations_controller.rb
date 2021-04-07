4d2ce6471370bda9b705d0dd34156a1d36fadec4
Gustavo Carvalho <gsc13@tuta.io>
2021-04-07 19:13:21 -0300

    Update registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:full_name, :username, :email, :password, :password_confirmation)
   end
 end
