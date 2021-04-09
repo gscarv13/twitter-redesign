c483b5365dec2c64ce1f12c1c2a3f00122c7ca5b
Gustavo Carvalho <gsc13@tuta.io>
2021-04-09 18:53:39 -0300

    Add follow/unfollow actions to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,29 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.order('created_at DESC')
   end
+
+  def follow
+    @user = User.find(params[:id])
+    @follow = current_user.followers.build(followed: @user)
+
+    if @follow.save
+      flash[:notice] = 'Following dood!'
+    else
+      flash[:alert] = 'Something went wrong... dood'
+    end
+
+    redirect_to users_show_path(@user)
+  end
+
+  def unfollow
+    @user = User.find(params[:id])
+    @follow = Following.find_by(followed: @user, follower: current_user)
+
+    if @follow.persisted?
+      @follow.destroy
+      flash[:notice] = 'Not a follower anymore dood!'
+    end
+
+    redirect_to users_show_path(@user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'users/:id', to: 'users#show', as: 'users_show'
+
+  get 'follow/:id', to: 'users#follow', as: 'user_follow'
+  get 'unfollow/:id', to: 'users#unfollow', as: 'user_unfollow'
+
   resources :posts
 end
