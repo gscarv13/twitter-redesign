8eb0bcb0323fc8bb5236edd53742c1618251d6ab
Gustavo Carvalho <gsc13@tuta.io>
2021-04-10 15:23:00 -0300

    Fix follow_buttons helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
-  def follow_buttons
-    if current_user.followers.where(followed: @user).pluck(:followed_id).any?
-      link_to user_unfollow_path(@user) do
+  def follow_buttons(user)
+    if current_user.followers.pluck(:followed_id).include?(user.id)
+      link_to user_unfollow_path(user) do
         '<span class="iconify" data-icon="ic:outline-cancel" data-inline="false"></span>'.html_safe
       end
     else
-      link_to user_follow_path(@user) do
+      link_to user_follow_path(user) do
         '<span class="iconify" data-icon="akar-icons:circle-plus" data-inline="false"></span>'.html_safe
       end
     end
