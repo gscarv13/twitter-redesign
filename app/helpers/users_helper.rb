064e258d59b6c267bc34d8f7be48dc3342f92ce3
Gustavo Carvalho <gsc13@tuta.io>
2021-04-11 17:55:14 -0300

    Add helpers to display user photo and cover_image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,4 +10,22 @@ module UsersHelper
       end
     end
   end
+
+  def user_photo(user)
+    if user.photo.file.nil?
+      img = 'https://res.cloudinary.com/sdcnwco/image/upload/v1618161614/prinny_ibxg3o.jpg'
+      "<img src='#{img}' alt='default-avatar' class='profile-img pr-3'>".html_safe
+    else
+      "<img src='#{user.photo}' alt='' class='profile-img pr-3'>".html_safe
+    end
+  end
+
+  def cover_image(user)
+    if user.cover_image.file.nil?
+      img = 'https://res.cloudinary.com/sdcnwco/image/upload/v1618165441/3_ymbgau.jpg'
+      "<img src='#{img}' alt='default-bg' class='cover-img'>".html_safe
+    else
+      "<img src='#{user.cover_image}' alt='' class='cover-img'>".html_safe
+    end
+  end
 end
