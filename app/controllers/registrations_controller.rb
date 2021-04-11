8f100ee4f0996ddd37ca50a7abe97650514e2ca6
Gustavo Carvalho <gsc13@tuta.io>
2021-04-11 17:54:38 -0300

    Add fields to upload images to edit user form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(:full_name, :username, :email, :password, :password_confirmation)
   end
+
+  def account_update_params
+    params.require(:user).permit(:full_name, :username, :email, :password, :password_confirmation,:current_password, :photo, :cover_image)
+  end
 end
