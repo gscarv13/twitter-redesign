190dee1127dc752758aa8a9b2180372b4acf08a1
Gustavo Carvalho <gsc13@tuta.io>
2021-04-11 17:52:59 -0300

    Add image upload feature

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'devise'
 gem 'simple_form', '~> 5.1'
+gem 'carrierwave'
+gem 'cloudinary'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
   has_many :followers, class_name: 'Following', foreign_key: 'follower_id'
   has_many :followed, class_name: 'Following', foreign_key: 'followed_id'
+
+  mount_uploader :photo, ImageUploader
+  mount_uploader :cover_image, ImageUploader
 end
